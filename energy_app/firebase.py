5259e00eb1dbffabff54a876df1bc6b2ca94915b
NaveenKrishna0312 <96708320+NaveenKrishna0312@users.noreply.github.com>
2025-02-09 13:38:30 +0530

    Delete energy_app directory

--- energy_app/firebase.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, db
-
-cred = credentials.Certificate("path_to_your_firebase_json.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://your-firebase-db.firebaseio.com'
-})
